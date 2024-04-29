5fc29692c211387a9d4623d607f87337f91578ae
SeptimeChampenois <49129002+SeptimeChampenois@users.noreply.github.com>
2024-04-29 21:47:04 +0200

    my_multiple_variables_multiple_type.py

---
+++ my_multiple_variables_multiple_type.py
@@ -0,0 +1,6 @@
+#my_multiple_variables_multiple_type.py
+#str to convert int to string
+my_age = 34;
+my_name = "Luke";
+my_comma = ',';
+print("Hello " + my_name + my_comma + " I'm "+ str(my_age) + " years old.")
