9a8bbf4b039111feb0b91582c3234acc7068aedc
SeptimeChampenois <49129002+SeptimeChampenois@users.noreply.github.com>
2024-05-09 17:36:12 +0200

    Create my_each.py

---
+++ Python-quest05/ex00/my_each.py
@@ -0,0 +1,11 @@
+def my_each(param_1):
+    index = 0
+    while index < len(param_1):
+        print(param_1[index])
+        index +=1
+
+#print(my_each(["hello", "today", "is", "tuesday"]))
+
+#def my_each(param_1):
+#    for i in range(len(param_1)):
+#        print((param_1)[i])
